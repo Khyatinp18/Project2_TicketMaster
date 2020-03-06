f577058695eec6d7af131bfbb7c2fe2f901c139a
khyatinp18 <khyatinp18@gmail.com>
2020-03-06 17:18:59 -0500

    API is working, Listed some stuff

--- Project2_TicketMaster/Controllers/TicketMasterController.cs
+++ Project2_TicketMaster/Controllers/TicketMasterController.cs
@@ -1,20 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Project2_TicketMaster.Models;
 
 namespace Project2_TicketMaster.Controllers
 {
     public class TicketMasterController : Controller
     {
         private readonly string ticketMasterApiKey;
-        public TicketMasterController(IConfiguration configuration)
+        private readonly HttpClient _client;
+
+        public TicketMasterController(IHttpClientFactory client, IConfiguration configuration)
         {
             ticketMasterApiKey = configuration.GetSection("APIKeys")["APINameKey"];
+            _client = client.CreateClient();
+            _client.BaseAddress = new Uri("https://app.ticketmaster.com/discovery/v2/");
         }
 
+        public async Task<IActionResult> GetEventList(int id)
+        {
+            var response = await _client.GetAsync($"attractions.json?apikey={ticketMasterApiKey}");
+            var events = await response.Content.ReadAsAsync<EventRootobject>();
+
+            return View(events);
+        }
 
         public IActionResult Index()
         {
--- Project2_TicketMaster/Startup.cs
+++ Project2_TicketMaster/Startup.cs
@@ -30,6 +30,7 @@ namespace Project2_TicketMaster
         {
             var connection = "Server=.\\SQLExpress;Database=EventFavourites;Trusted_Connection=True;ConnectRetryCount=0;";
 
+            services.AddHttpClient();
             services.AddDbContext<EventFavouritesContext>(
             options => options.UseSqlServer(connection)
             );
